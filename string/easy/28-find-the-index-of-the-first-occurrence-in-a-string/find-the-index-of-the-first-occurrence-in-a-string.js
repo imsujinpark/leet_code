cfa025392dc4d54c197e2281375db3cfe1768b36
Sujin Park <imsujinpark@gmail.com>
2024-03-03 23:51:20 +0900

    Time: 60 ms (19.84%) | Memory: 49.1 MB (18.83%) - LeetSync

--- string/easy/28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
+++ string/easy/28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
@@ -11,14 +11,17 @@ var strStr = function(haystack, needle) {
     let needleIndex = 0;
 
     for (let i = 0; i <= haystack.length; i++) {
+        // If characters match, move to the next character in both needle and haystack
         if (needle[needleIndex] === haystack[i]) {
             needleIndex++;
 
+            // If all characters in needle are found in sequence, return the starting index
             if (needleIndex === needle.length) {
                 return i - needle.length + 1;
             }
 
         } else {
+            // If characters don't match, reset the iterator and needle index
             i -= needleIndex;
             needleIndex = 0;
         }
