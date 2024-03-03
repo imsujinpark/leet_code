a7144aeab7a12e4e55e5d05ffe118468201b38f5
Sujin Park <imsujinpark@gmail.com>
2024-03-03 23:43:51 +0900

    Time: 51 ms (67.13%) | Memory: 48.9 MB (38.74%) - LeetSync

---
+++ string/easy/28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    if (needle.length > haystack.length) {
+        return -1;
+    }
+
+    let needleIndex = 0;
+
+    for (let i = 0; i <= haystack.length; i++) {
+        if (needle[needleIndex] === haystack[i]) {
+            needleIndex++;
+
+            if (needleIndex === needle.length) {
+                return i - needle.length + 1;
+            }
+
+        } else {
+            i -= needleIndex;
+            needleIndex = 0;
+        }
+    }
+
+    return -1;
+};
