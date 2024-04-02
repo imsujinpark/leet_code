6b1e21491cc2d71afdc2b55c1cb5ff791071b2cb
Sujin Park <imsujinpark@gmail.com>
2024-04-02 17:36:12 +0900

    Time: 64 ms (74.41%) | Memory: 53.9 MB (25.96%) - LeetSync

--- string/easy/205-isomorphic-strings/isomorphic-strings.ts
+++ string/easy/205-isomorphic-strings/isomorphic-strings.ts
@@ -1,22 +1,23 @@
 function isIsomorphic(s: string, t: string): boolean {
     // a map where I will keep each character of s as key and t as value
-    const mapS: Map<string, string> = new Map();
-    const mapT: Map<string, string> = new Map();
+    const stringMap: Map<string, string> = new Map();
 
     for (let i = 0; i < s.length; i++) {
         // if there is no value for the character
         // set the key as s[i] and value as t[i]
         // only if t[i] is not used in other keys
-        if (!mapS.has(s[i])) {
-            mapS.set(s[i], t[i]);
-        } else if (mapS.get(s[i]) !== t[i]) {
-            return false
+        if (!stringMap.has(s[i])) {
+            if (Array.from(stringMap.values()).includes(t[i])) {
+                return false;
+            }
+
+            stringMap.set(s[i], t[i]);
+            continue;
         }
         
-        if (!mapT.has(t[i])) {
-            mapT.set(t[i], s[i]);
-        } else if (mapT.get(t[i]) !== s[i]) {
-            return false
+        // if there is a value already set, check if it matches the one of t at the same index
+        if (stringMap.get(s[i]) !== t[i]) {
+            return false;
         }
     }
 
