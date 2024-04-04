f04766f7f45928325ab5e91a0e1cacbc3c012d74
Sujin Park <imsujinpark@gmail.com>
2024-04-04 11:02:46 +0900

    Time: 50 ms (89.02%) | Memory: 51.2 MB (89.02%) - LeetSync

--- string/easy/1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.ts
+++ string/easy/1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.ts
@@ -2,14 +2,19 @@ function maxDepth(s: string): number {
     let maxDepth = 0;
     let parenthesisMarker = 0;
 
+    // loop the given string s
     for(let i = 0; i < s.length; i++) {
+        // mark the number of opening parenthesis
         if (s[i] === '(') {
             parenthesisMarker++;
         } else if (s[i] === ')') {
-            maxDepth = parenthesisMarker > maxDepth ? parenthesisMarker : maxDepth;
+            // mark the number of closing parenthesis
+            // mark the maximum depth only if it's bigger than the current max
+            maxDepth = Math.max(maxDepth, parenthesisMarker);
             parenthesisMarker--;
         }
     }
 
+    // if parenthesisMarker is not 0, it means the parenthesis are not closed properly so return 0
     return parenthesisMarker !== 0 ? 0 : maxDepth;
 };
