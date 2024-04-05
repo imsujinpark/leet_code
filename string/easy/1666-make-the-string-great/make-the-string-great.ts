4ef632014cf1220306d0f22cd8fd628395fc37dd
Sujin Park <imsujinpark@gmail.com>
2024-04-05 12:45:55 +0900

    Time: 57 ms (85.00%) | Memory: 52.4 MB (38.00%) - LeetSync

--- string/easy/1666-make-the-string-great/make-the-string-great.ts
+++ string/easy/1666-make-the-string-great/make-the-string-great.ts
@@ -1,28 +1,20 @@
 function makeGood(s: string): string {
-    // this function will either return number of bad char or null if there is no bad char
-    function getBadCharStartingIndex(s: string): number|null {
-        for (let i = 0; i < s.length - 1; i++) {
-            // check if s[i+1] is an upper case of s[i] or vice-versa
-            if (Math.abs(s.charCodeAt(i) - s.charCodeAt(i+1)) === 32) {
-                return i;
+    let i = 0;
+
+    while (i < s.length && s[i + 1]) {
+        if (Math.abs(s.charCodeAt(i) - s.charCodeAt(i+1)) === 32) {
+            s = s.substring(0, i) + s.substring(i + 2);
+
+            // if it was the first char, start from 0
+            if (i === 0) {
+                i = -1;
+            // if not, go back two steps to see if adjacent chars are the same
+            } else {
+                i -= 2;
             }
         }
-
-        return null;
+        i++;
     }
-
-    function removeBadStrings(s: string): string {
-        const badCharStartingIndex = getBadCharStartingIndex(s);
-        
-        if (badCharStartingIndex !== null) {
-            s = s.substring(0, badCharStartingIndex) + s.substring(badCharStartingIndex + 2);
-
-            // untill there is no bad char, iterate
-            return removeBadStrings(s);
-        }
-
-        return s;
-    }
-
-    return removeBadStrings(s);
+    
+    return s;
 };
