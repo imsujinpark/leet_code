c1015da91289fa1f3c2f6d2ebfdaf07aa76742f4
Sujin Park <imsujinpark@gmail.com>
2024-04-05 12:29:44 +0900

    Time: 72 ms (17.00%) | Memory: 52.5 MB (28.00%) - LeetSync

---
+++ string/easy/1666-make-the-string-great/make-the-string-great.ts
@@ -0,0 +1,27 @@
+function makeGood(s: string): string {
+    // this function will either return number of bad char or null if there is no bad char
+    function getBadCharStartingIndex(s: string): number|null {
+        for (let i = 0; i < s.length - 1; i++) {
+            // check if s[i+1] is an upper case of s[i] or vice-versa
+            if (Math.abs(s.charCodeAt(i) - s.charCodeAt(i+1)) === 32) {
+                return i;
+            }
+        }
+
+        return null;
+    }
+
+    function removeBadStrings(s: string): string {
+        const badCharStartingIndex = getBadCharStartingIndex(s);
+        
+        if (badCharStartingIndex !== null) {
+            s = s.substring(0, badCharStartingIndex) + s.substring(badCharStartingIndex + 2);
+
+            return removeBadStrings(s);
+        }
+
+        return s;
+    }
+
+    return removeBadStrings(s);
+};
